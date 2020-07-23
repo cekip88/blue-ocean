f7f271771ce48ffb1a4fd8cbf6fd8cad0d22e33b
Антон Сметанин <foremost186@gmail.com>
2020-07-23 18:03:32 +0600

    nav menu add submenu, almost finish work for tablet

--- dev/js/main.js
+++ dev/js/main.js
@@ -30,20 +30,35 @@ class Site {
                 _.header.classList.add('head-scrolled')
             }
         } else {
+            _.header.classList.remove('head-mobile-row-hidden');
             if(!window.pageYOffset){
                 _.header.classList.remove('head-scrolled')
             }
         }
     }
+
+    // Показывает подменю продуктов
+    productClick(){
+        const _ = this;
+        _.header.classList.toggle('head-mobile-row-hidden')
+    }
 }
 
 let site = new Site();
 let burgerButton = document.querySelector('.head-burger-button')
+let productButton = document.querySelector('.head-nav-product')
 
 
-burgerButton.addEventListener('click',function () {
+
+burgerButton.addEventListener('click',function (e) {
     site.burgerClick()
 });
+
+productButton.addEventListener('click',function (e) {
+    e.preventDefault();
+    site.productClick()
+});
+
 window.addEventListener('scroll',function () {
     site.scrl();
 },{passive:true});
