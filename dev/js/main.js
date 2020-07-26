75e0545896baa39be04369c9c00fe6444f8974de
Антон Сметанин <foremost186@gmail.com>
2020-07-26 18:17:24 +0600

    sunday finished

--- dev/js/main.js
+++ dev/js/main.js
@@ -42,12 +42,26 @@ class Site {
         const _ = this;
         _.header.classList.toggle('head-mobile-row-hidden')
     }
+
+    // Показывает сертификат полностью
+    sertClick(e){
+        const _ = this;
+        let clickTarget = e.target;
+        if(clickTarget.className != 'sertificates-item'){
+            while(clickTarget.className != 'sertificates-item'){
+                clickTarget = clickTarget.parentNode;
+            }
+        }
+        clickTarget = clickTarget.querySelector('.sertificates-full');
+        clickTarget.classList.toggle('sertificates-full-show');
+    }
 }
 
 let site = new Site();
-let burgerButton = document.querySelector('.head-burger-button')
-let productButton = document.querySelector('.head-nav-product')
-
+let burgerButton = document.querySelector('.head-burger-button');
+let productButton = document.querySelector('.head-nav-product');
+let sertButton = document.querySelectorAll('.sertificates-link');
+let sertClose = document.querySelectorAll('.sertificates-full-close');
 
 
 burgerButton.addEventListener('click',function (e) {
@@ -58,6 +72,20 @@ productButton.addEventListener('click',function (e) {
     e.preventDefault();
     site.productClick()
 });
+
+sertButton.forEach(function (el) {
+    el.addEventListener('click',function (e) {
+        e.preventDefault();
+        site.sertClick(e);
+    })
+});
+sertClose.forEach(function (el) {
+    el.addEventListener('click',function (e) {
+        e.preventDefault();
+        site.sertClick(e);
+    })
+});
+
 
 window.addEventListener('scroll',function () {
     site.scrl();
