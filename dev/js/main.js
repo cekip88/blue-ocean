16486325376432445a1593b46a115b307d7cfe7b
Антон Сметанин <foremost186@gmail.com>
2020-08-06 12:10:17 +0600

    Вторник короче finished

--- dev/js/main.js
+++ dev/js/main.js
@@ -4,7 +4,9 @@ class Site {
         this.burgerButton = document.querySelector('.head-burger');
         this.headNavProductButton = document.querySelector('.head-nav-product');
         this.sertButton = document.querySelectorAll('.sertificates-link');
-        this.sertBg = document.querySelectorAll('.sertificates-full');
+        this.sertClose = document.querySelectorAll('.sertificates-full-close');
+        this.articleButton = document.querySelectorAll('.articles-unit-open');
+        this.articleButtonClose = document.querySelectorAll('.articles-unit-close');
     }
 
     // При скролле добавляет или убирает класс scrolled хедеру
@@ -66,10 +68,99 @@ class Site {
         cont.classList.toggle('sertificates-full-show');
     }
 
+    // Считывает высоты скрытой и показанной частей статьи и записывает ее в аттрибут
+    articlesGetHeight(){
+        let articles = document.querySelectorAll('.articles-unit-hidden');
+        if(articles){
+            let asdf = function () {
+                articles.forEach(function (item) {
+                    let itemChildren = item.children;
+                    let itemHeight = 0;
+                    let mul  = itemChildren.length * 20;
+                    for(let i = 0; i < itemChildren.length; i++){
+                        itemHeight += itemChildren[i].offsetHeight;
+                    }
+                    itemHeight += mul;
+                    item.setAttribute('fullHeight',`${itemHeight}`);
+                });
+                let articlesShow = document.querySelectorAll('.articles-unit-show');
+                articlesShow.forEach(function (item) {
+                    item.setAttribute('fullHeight',`${item.offsetHeight}`);
+                    item.setAttribute('style',`height:${item.offsetHeight}px`)
+                })
+            };
+            setTimeout(asdf,500)
+        }
+    }
+    // Метод плавного скролла на верх
+    slowScrollTo(start,finish){
+        let sLength = start,
+            step = ((start - finish) / 100);
+        let func = function(){
+            sLength = sLength - step;
+            window.scrollTo(0, sLength)
+        };
+        console.log(sLength,step);
+        while(sLength > finish){
+            setInterval(func,1)
+        }
+    }
+    // Метод который разворачивает статьи
+    articlesShowFull(e){
+        const _ = this;
+        let article = e.target;
+        while(article.className != 'articles-unit'){
+            article = article.parentElement;
+        }
+
+        _.slowScrollTo(window.scrollY,article.offsetTop);
+
+        let hidden = article.querySelector('.articles-unit-hidden'),
+            hiddenHeight = hidden.getAttribute('fullHeight');
+        hidden.setAttribute('style',`height:${hiddenHeight}px`);
+        let showHidden = function(){
+            let show = article.querySelector('.articles-unit-show');
+            show.setAttribute('style','height:0px');
+        };
+        setTimeout(showHidden,350);
+    }
+    // Метод который сворачивает статьи
+    articlesCloseFull(e){
+        let article = e.target;
+        while(article.className != 'articles-unit'){
+            article = article.parentElement;
+        }
+        window.scrollTo(0, article.offsetTop);
+        let show = article.querySelector('.articles-unit-show'),
+            showHeight = show.getAttribute('fullHeight');
+        show.setAttribute('style',`height:${showHeight}px`);
+        let showHidden = function(){
+            let hidden = article.querySelector('.articles-unit-hidden');
+            hidden.setAttribute('style',`height:0px`);
+        };
+        setTimeout(showHidden,350);
+    }
 
     //Метод который вешает обработчики на кнопки
     eHandler(){
         const _ = this;
+        // Вешает обработчик на кнопки показа статьи
+        if(_.articleButton){
+            _.articleButton.forEach(function (el) {
+                el.addEventListener('click', (e) => {
+                    e.preventDefault();
+                    _.articlesShowFull(e)
+                })
+            });
+        }
+        if(_.articleButtonClose){
+            _.articleButtonClose.forEach(function (el) {
+                el.addEventListener('click', (e) => {
+                    e.preventDefault();
+                    _.articlesCloseFull(e)
+                })
+            })
+        }
         // Вешает обработчик на кнопку бургера
         if(this.burgerButton){
             this.burgerButton.addEventListener('click',(e) => {
@@ -86,7 +177,7 @@ class Site {
         }
         // Вешает обработчики на сертификаты
         if(this.sertButton){
-            [...this.sertButton, ...this.sertBg].forEach((el) => {
+            [...this.sertButton, ...this.sertClose].forEach((el) => {
                 el.addEventListener('click',(e) => {
                     e.preventDefault();
                     _.sertClick(e);
@@ -104,6 +195,7 @@ class Site {
         if(document.querySelector('.sertificates-full')){
             this.sertRemoveAttr();
         }
+        this.articlesGetHeight();
         this.eHandler();
     }
 }
